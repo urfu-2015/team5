005ee112ceffba9776ed8dcf5e903c6446e91d57
hx0day <hx0day@hackerdom.ru>
2016-03-13 14:40:45 +0500

    feat(views): init config

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const config = require('config');
 const hbs = require('hbs');
 
 var app = express();
