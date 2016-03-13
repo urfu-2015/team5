f16cf51ce4da9fd6ec9d5c2ef6188b3728de7f81
Надежда <grin.nadezh@yandex.ru>
2016-03-13 14:54:15 +0500

    Quest page start
    Quests.js + Quests.hbs

--- controllers/quests.js
+++ controllers/quests.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var hbs_helper = require('../utils/handlebars_util.js')();
+
 exports.list = function (req, res) {
+    console.log(req.param.name);
     res.render('quests/list', {
         quests: [{
             name: 'first_quest',
@@ -64,4 +67,38 @@ exports.list = function (req, res) {
             url: 'http://f9.mirkvartir.me/1024x768/95/9524b66c-3554-499a-b21f-231383fbcf25.jpg'
         }]
     });
+};
+
+exports.content = function (req, res) {
+    console.log(req.param('name'));
+    res.render('quests/quest', {
+        name: req.param('name'),
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
+        'Morbi nec vehicula sem, eget faucibus magna. Nam sapien est, cursus vel elementum' +
+        ' at, finibus ac ipsum. Phasellus ut quam id sem ullamcorper tincidunt. Vestibulum' +
+        ' a magna eros. Nunc quam arcu, egestas nec felis ut, hendrerit volutpat nibh. ' +
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia ' +
+        'Curae; Nullam odio massa, finibus nec tellus in, mattis varius metus. Sed vitae nibh' +
+        ' at sapien euismod semper. Donec rutrum elementum diam nec egestas. Duis interdum,' +
+        ' tellus non sollicitudin commodo, tellus diam aliquet nulla,' +
+        ' a maximus mauris mauris id enim.',
+        pictures: [
+            {
+                url: 'http://travelucion.ru/Yekaterinburg/Yekaterinburg-84147.JPG',
+                name: 'First photo'
+            }, {
+                url: 'http://www.2do2go.ru/uploads/full/b627d145dd2f6905549f259eaa4d584a_w960_h2048.jpg',
+                name: 'Second photo'
+            }, {
+                url: 'http://www.2do2go.ru/uploads/full/b627d145dd2f6905549f259eaa4d584a_w960_h2048.jpg',
+                name: 'Third photo'
+            }, {
+                url: 'http://www.2do2go.ru/uploads/full/b627d145dd2f6905549f259eaa4d584a_w960_h2048.jpg',
+                name: 'Fourth photo'
+            }, {
+                url: 'http://www.2do2go.ru/uploads/full/b627d145dd2f6905549f259eaa4d584a_w960_h2048.jpg',
+                name: 'Fifth photo'
+            }
+        ]
+    })
 };
--- routes.js
+++ routes.js
@@ -3,18 +3,19 @@
 var passport = require('passport');
 var index = require('./controllers/index');
 var auth = require('./controllers/auth');
-var quest = require('./controllers/quests');
+var quests = require('./controllers/quests');
 
 module.exports = function (app) {
-	        app.post('/login', passport.authenticate('local', {
-				successRedirect: '/',
-				failureRedirect: '/login',
-				failureFlash: true
-				}));
-	        app.get('/login', auth.loginPage);
-	        app.post('/register', auth.register);
-	        app.get('/register', auth.registerPage);
-	        app.get('/logout', auth.logout);
-	        app.get('/quests', quest.list);
-	        app.get('/', index.index);
+	app.post('/login', passport.authenticate('local', {
+		successRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	}));
+	app.get('/login', auth.loginPage);
+	app.post('/register', auth.register);
+	app.get('/register', auth.registerPage);
+	app.get('/logout', auth.logout);
+	app.get('/', index.index);
+	app.get('/quests', quests.list);
+	app.get('/quests/:name', quests.content);
 };
