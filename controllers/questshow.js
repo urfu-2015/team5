edfd62d50e83b97b7b949aeaab8fa3d2f3b173b5
alexander.vakhitov <deadhunter007@live.ru>
2016-04-17 17:03:22 +0500

    feat(questshow): Контроллер для квеста.

--- controllers/questshow.js
+++ controllers/questshow.js
@@ -4,5 +4,28 @@ var Quest = require('./../models/quest');
 var Picture = require('./../models/picture');
 
 exports.show = function (req, res) {
+    Quest.findById(req.params.id, function (error, quest) {
+        if (error) {
+            console.error(error);
+            return;
+        }
 
+        var pictures = [];
+        quest.pictures.forEach(function (item) {
+            Picture.findById(item, function (error, pic) {
+                pictures.push({
+                    name: pic.name,
+                    description: pic.description,
+                    url: pic.url
+                });
+            });
+        });
+
+        res.render('', {
+            name: quest.name,
+            description: quest.description,
+            url: quest.cover,
+            pictures: pictures
+        });
+    });
 };
