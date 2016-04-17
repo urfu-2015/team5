fb2c22dd8e465324dd4ff9cf82475ca9341b3d8e
Michael Uskov <uskovm@gmail.com>
2016-04-17 16:56:33 +0500

    Улучшение формы

--- bundles/addquest/addquest.js
+++ bundles/addquest/addquest.js
@@ -1,11 +1,52 @@
 ﻿var createPhotoDiv = function () {
     var newPhotoDiv = $('<div>');
+    var hasMadeAnotherDiv;
     newPhotoDiv.html($('.addquest__template').html());
+    newPhotoDiv.find('.addquest__removephoto').click(function () {
+        newPhotoDiv.remove();
+    });
+    newPhotoDiv.find('.addquest__picInput').change(function () {
+        if (!hasMadeAnotherDiv) {
+            hasMadeAnotherDiv = true;
+            appendPhotoDiv();
+        }
+    });
+    var idPrefix = Date.now() + '-';
+    newPhotoDiv.children().filter(function (i, el) {
+        return $(el).attr('id') != '';
+    }).each(function (i, el) {
+        $(el).attr('id', idPrefix + $(el).attr('id'));
+    });
     return newPhotoDiv;
 };
 
-var onLoad = function () {
-    $('.addquest__photoplace').append(createPhotoDiv());  
+var setArrayIndicesForChildrens = function (element, index) {
+    $(element).children().filter(function (i, el) {
+        return $(el).attr('name') != '';
+    }).each(function (i, el) {
+        $(el).attr('name', $(el).attr('name') + '[' + index + ']');
+    });
+};
+
+var setArrayIndicesForPhotos = function () {
+    $('.addquest__photoplace').children().each(function (i, el) {
+        setArrayIndicesForChildrens(el, i);
+    });
+};
+
+var appendPhotoDiv = function (div) {
+     $('.addquest__photoplace').append(div);
+};
+
+var onLoad = function () {  
+    if (!$('.addquest__form').length) {
+        return;
+    }
+    $('.addquest__addphoto').click(function () {
+        appendPhotoDiv(createPhotoDiv());
+    });
+    appendPhotoDiv(createPhotoDiv());
+    $('.addquest__form').submit(setArrayIndicesForPhotos);
 };
 
 window.addEventListener('load', onLoad);
