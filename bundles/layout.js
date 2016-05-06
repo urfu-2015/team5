3d6a5d6ec0eb898b0e9f53ee11055653ed7e7d16
Надежда <grin.nadezh@yandex.ru>
2016-05-06 22:51:42 +0500

    Расположение menu, footer, header. Стили: начало.

--- bundles/layout.js
+++ bundles/layout.js
@@ -1,8 +1,10 @@
 require('../blocks/header/header.styl');
 require('../blocks/footer/footer.styl');
+require('../blocks/menu/menu.styl');
 require('./error/error.styl');
 require('./index/index.styl');
 require('./quests/quests.styl');
 require('./addquest/addquest.styl');
 require('./quest/quest.styl');
 require('./addquest/addquest.js');
+require('./layout.styl');
