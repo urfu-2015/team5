0897d1e791d9dedf837a31cf45e7dffa9853d8a0
Michael Uskov <uskovm@gmail.com>
2016-05-15 14:18:36 +0500

    Добавить файл quest.js

---
+++ bundles/quest/quest.js
@@ -0,0 +1,15 @@
+﻿function removeQuest() {
+    var id = $('.quest').data('id');
+    $.post({
+        url: '/quests/remove/' + id,
+        success: onRemoveSuccess
+    }); 
+}
+
+function onRemoveSuccess() {
+    document.location = '/quests';
+}
+
+$(window).load(function () {
+    $('.quest__manage__remove').click(removeQuest);
+});
